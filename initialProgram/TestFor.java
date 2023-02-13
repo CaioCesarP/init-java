4b53df83eb41b3397f540ba5b2a6163c69b82b18
caio.cp <caio.cesar@itarget.com.br>
2023-02-13 11:23:51 -0300

    :recycle: refactor: separando por projetos.

---
+++ initialProgram/TestFor.java
@@ -0,0 +1,14 @@
+package initialProgram;
+public class TestFor {
+
+  public static void main(String[] args) {
+
+    for (int counter = 0; counter <= 10; counter++) {
+
+      System.out.println(counter);
+
+    }
+
+  }
+
+}
---
+++ initialProgram/TestWhile.java
@@ -0,0 +1,17 @@
+package initialProgram;
+public class TestWhile {
+
+  public static void main(String[] args) {
+
+    int counter = 0;
+
+    while (counter <= 10) {
+
+      System.out.println(counter);
+      counter++;
+
+    }
+
+  }
+
+}
