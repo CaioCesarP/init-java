7f4ba68fee51bf530de01e6990da436adb0751ac
caio.cp <caio.cesar@itarget.com.br>
2023-02-13 10:48:29 -0300

    :tada: feat: commit inicial de projetos java.

---
+++ Program.java
@@ -0,0 +1,100 @@
+class Program {
+
+    public static void main(String[] args) {
+
+        // Club problem
+
+        // int age = 18;
+        // int sizeOfGroup = 3;
+        // boolean accompanied = sizeOfGroup >= 2;
+        // int peopleInsideClub = 25;
+
+        // if (age < 18 && !accompanied) {
+        // System.out.println("You aren't of legal age and is not accompanied.");
+        // return;
+        // }
+
+        // peopleInsideClub += sizeOfGroup;
+        // if (peopleInsideClub >= 30) {
+        // System.out.println("The club is packed.");
+        // peopleInsideClub -= sizeOfGroup;
+        // return;
+        // }
+
+        // System.out.println("You may come in. Welcome.");
+
+        // IR problem
+
+        // double salary = 3300.0;
+
+        // if (2800.0 >= salary && salary >= 1900.0) {
+        // System.out.println("o IR é de 7.5% e pode deduzir na declaração o valor de R$
+        // 142");
+        // return;
+        // }
+
+        // if (3751.0 >= salary && salary > 2800.0) {
+        // System.out.println("o IR é de 15% e pode deduzir R$ 350");
+        // return;
+        // }
+
+        // if (4664.0 >= salary && salary > 3751.0) {
+        // System.out.println("o IR é de 22.5% e pode deduzir R$ 636");
+        // return;
+        // }
+
+        // headquarters problem
+
+        // for (int i = 1; i <= 10; i++) {
+        // for (int j = 1; j <= 10; j++) {
+
+        // if (i != j) {
+        // continue;
+        // }
+
+        // System.out.println(i + " " + j);
+
+        // }
+        // }
+
+        // table problem
+
+        // for (int multiplicative = 1; multiplicative <= 10; multiplicative++) {
+        // System.out.println("Tabela do " + multiplicative);
+        // for (int counter = 1; counter <= 10; counter++) {
+        // System.out.print(multiplicative * counter + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // break problem
+
+        // for (int row = 0; row <= 10; row++) {
+        // for (int column = 0; column <= 10; column++) {
+        // if (column > row)
+        // break;
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // multiple of 3
+
+        // for (int counter = 1; counter <= 100; counter++) {
+        // if (counter % 3 == 0) {
+        // System.out.print(counter + " ");
+        // }
+        // }
+        // System.out.println();
+
+        // factorial problem
+
+        // int result = 1;
+        // for (int value = 1; value <= 10; value++) {
+        // result *= value;
+        // System.out.println("Fatorial de " + value + " = " + result);
+        // }
+
+    }
+
+}
---
+++ TestFor.java
@@ -0,0 +1,13 @@
+public class TestFor {
+
+  public static void main(String[] args) {
+
+    for (int counter = 0; counter <= 10; counter++) {
+
+      System.out.println(counter);
+
+    }
+
+  }
+
+}
